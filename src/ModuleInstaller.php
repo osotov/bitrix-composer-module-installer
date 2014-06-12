a1b0435fa60f2e9d8ed6226a77715cfb4a242041
Sapth <reghan7@gmail.com>
2014-06-12 14:21:53 +0400

    initial commit

---
+++ src/ModuleInstaller.php
@@ -0,0 +1,32 @@
+<?php
+namespace Bitrix\Composer;
+
+use Composer\Package\PackageInterface;
+use Composer\Installer\LibraryInstaller;
+
+class ModuleInstaller {
+	/**
+	 * {@inheritDoc}
+	 */
+	public function getPackageBasePath(PackageInterface $package)
+	{
+		$prefix = substr($package->getPrettyName(), 0, 23);
+		if ('phpdocumentor/template-' !== $prefix) {
+			throw new \InvalidArgumentException(
+				'Unable to install template, phpdocumentor templates '
+				.'should always start their package name with '
+				.'"phpdocumentor/template-"'
+			);
+		}
+
+		return 'data/templates/'.substr($package->getPrettyName(), 23);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public function supports($packageType)
+	{
+		return 'phpdocumentor-template' === $packageType;
+	}
+} 
---
+++ src/ModuleInstallerPlugin.php
@@ -0,0 +1,16 @@
+<?php
+namespace Bitrix\Composer;
+
+use Composer\Composer;
+use Composer\IO\IOInterface;
+use Composer\Plugin\PluginInterface;
+
+
+class ModuleInstallerPlugin implements PluginInterface {
+
+	public function activate(Composer $composer, IOInterface $io)
+	{
+		$installer = new ModuleInstaller($io, $composer);
+		$composer->getInstallationManager()->addInstaller($installer);
+	}
+} 

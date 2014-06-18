c2cedebea58405593440f3d70914c31d6793ad82
Sapth <reghan7@gmail.com>
2014-06-19 00:54:43 +0400

    added some conditions

--- src/Bitrix/Composer/ModuleInstaller.php
+++ src/Bitrix/Composer/ModuleInstaller.php
@@ -5,23 +5,29 @@ use Composer\Package\PackageInterface;
 use Composer\Installer\LibraryInstaller;
 
 class ModuleInstaller extends LibraryInstaller {
+	const PACKAGE_TYPE = 'bitrix-module-installer';
 	/**
 	 * {@inheritDoc}
 	 */
 	public function getPackageBasePath(PackageInterface $package)
 	{
 		$extras = $this->composer->getPackage()->getExtra();
+		$packageType = $package->getType();
+		if ($packageType == self::PACKAGE_TYPE) {
+			if ((array_key_exists('bitrix_module_name', $extras)) && (! empty($extras['bitrix_module_name']))) {
+				$name = (string) $extras['bitrix_module_name'];
+			} else {
+				throw new \Exception(
+					'Unable to install module, composer.json must contain module name declaration like this: ' .
+					'"extra": { "bitrix_module_name": "somename" } '
+				);
+			}
 
-		if ((array_key_exists('bitrix_module_name', $extras)) && (! empty($extras['bitrix_module_name']))) {
-			$name = (string) $extras['bitrix_module_name'];
-		} else {
-			throw new \Exception(
-				'Unable to install module, composer.json must contain module name declaration like this: ' .
-				'"extra": { "bitrix_module_name": "somename" } '
-			);
+			return 'local/modules/' . $name;
 		}
 
-		return 'local/modules/' . $name;
+
+		return '/vendor/' . $package->getPrettyName();
 	}
 
 	/**
@@ -29,6 +35,6 @@ class ModuleInstaller extends LibraryInstaller {
 	 */
 	public function supports($packageType)
 	{
-		return 'bitrix-module-installer' === $packageType;
+		return self::PACKAGE_TYPE === $packageType;
 	}
 } 

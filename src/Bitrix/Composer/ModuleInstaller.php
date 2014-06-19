fd5a339532cc9df1b5802011a483d9eb214a3b74
Sapth <reghan7@gmail.com>
2014-06-20 00:09:49 +0400

    bugfix

--- src/Bitrix/Composer/ModuleInstaller.php
+++ src/Bitrix/Composer/ModuleInstaller.php
@@ -16,16 +16,10 @@ class ModuleInstaller extends LibraryInstaller {
 		if ($packageType == self::PACKAGE_TYPE) {
 			if ((array_key_exists('bitrix_module_name', $extras)) && (! empty($extras['bitrix_module_name']))) {
 				$name = (string) $extras['bitrix_module_name'];
-			} else {
-				throw new \Exception(
-					'Unable to install module, composer.json must contain module name declaration like this: ' .
-					'"extra": { "bitrix_module_name": "somename" } '
-				);
+
+				return 'local/modules/' . $name;
 			}
-
-			return 'local/modules/' . $name;
 		}
-
 
 		return '/vendor/' . $package->getPrettyName();
 	}
